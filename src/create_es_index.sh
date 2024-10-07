2e355dead4991c851c3dbca1abcc516391a5b384
Mriganka Dhar <mrigankodhar0001@gmail.com>
2024-10-07 10:15:14 +0530

    Update index for Elastic Search

--- src/create_es_index.sh
+++ src/create_es_index.sh
@@ -1,24 +1,73 @@
 #!/bin/bash
 
-# Create movies index with appropriate mappings for movie name, director, and genre
-curl -X PUT "http://localhost:9200/movies" -H 'Content-Type: application/json' -d'
+# Create an index with the required mapping
+curl -X PUT "localhost:9200/movies" -H 'Content-Type: application/json' -d'
 {
   "mappings": {
     "properties": {
-      "adult": { "type": "boolean" },
-      "backdrop_path": { "type": "text" },
-      "genre_ids": { "type": "integer" },
+      "budget": { "type": "integer" },
+      "genres": {
+        "type": "nested",
+        "properties": {
+          "id": { "type": "integer" },
+          "name": { "type": "text" }
+        }
+      },
+      "homepage": { "type": "text" },
       "id": { "type": "integer" },
+      "keywords": {
+        "type": "nested",
+        "properties": {
+          "id": { "type": "integer" },
+          "name": { "type": "text" }
+        }
+      },
       "original_language": { "type": "text" },
       "original_title": { "type": "text" },
       "overview": { "type": "text" },
       "popularity": { "type": "float" },
-      "poster_path": { "type": "text" },
+      "production_companies": {
+        "type": "nested",
+        "properties": {
+          "name": { "type": "text" },
+          "id": { "type": "integer" }
+        }
+      },
+      "production_countries": {
+        "type": "nested",
+        "properties": {
+          "iso_3166_1": { "type": "text" },
+          "name": { "type": "text" }
+        }
+      },
       "release_date": { "type": "date" },
-      "title": { "type": "text" },
-      "video": { "type": "boolean" },
+      "revenue": { "type": "long" },
+      "runtime": { "type": "integer" },
+      "spoken_languages": {
+        "type": "nested",
+        "properties": {
+          "iso_639_1": { "type": "text" },
+          "name": { "type": "text" }
+        }
+      },
+      "status": { "type": "text" },
+      "tagline": { "type": "text" },
+      "title_x": { "type": "text" },
       "vote_average": { "type": "float" },
-      "vote_count": { "type": "integer" }
+      "vote_count": { "type": "integer" },
+      "title_y": { "type": "text" },
+      "cast": {
+        "type": "nested",
+        "properties": {
+          "cast_id": { "type": "integer" },
+          "character": { "type": "text" },
+          "credit_id": { "type": "text" },
+          "gender": { "type": "integer" },
+          "id": { "type": "integer" },
+          "name": { "type": "text" },
+          "order": { "type": "integer" }
+        }
+      }
     }
   }
 }'
