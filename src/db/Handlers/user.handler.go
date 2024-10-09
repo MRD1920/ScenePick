551b0b7e03b14c0cc0c16d385189c77a7da444ae
Mriganka Dhar <mrigankodhar0001@gmail.com>
2024-10-09 11:59:25 +0530

    Add user handler and user model

---
+++ src/db/Handlers/user.handler.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"context"
+	"time"
+
+	models "github.com/mrd1920/ScenePick/src/db/Models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func findOneUser(mongoClient *mongo.Client, email string) (*models.User, error) {
+	//Get the collection
+	collection := mongoClient.Database("ScenePick").Collection("Users")
+
+	//Find the user with the given email
+	var user models.User
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result := collection.FindOne(ctx, models.User{Email: email})
+	if result.Err() == mongo.ErrNoDocuments {
+		return nil, mongo.ErrNoDocuments
+	}
+
+	err := result.Decode((&user))
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func insertOneUser(mongoClient *mongo.Client, newUser models.User) (*mongo.InsertOneResult, error) {
+	//Get the collection
+	collection := mongoClient.Database("ScenePick").Collection("Users")
+
+	//Insert the new user
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	result, err := collection.InsertOne(ctx, newUser)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+
+}
---
+++ src/db/Models/user.model.go
@@ -0,0 +1,12 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty"`                               // MongoDB ObjectID
+	Name            string             `bson:"name" json:"name" binding:"required:"`        // Name of the user
+	Email           string             `bson:"email" json:"email" binding:"required:"`      // Email of the user
+	Password        string             `bson:"password" json:"password" binding:"required"` // Password of the user
+	FavouriteMovies []int              `bson:"favourite_movies" json:"favourite_movies"`    // List of favourite movie IDs
+	WatchLater      []int              `bson:"watch_later" json:"watch_later"`              // List of movie IDs to watch later
+}
